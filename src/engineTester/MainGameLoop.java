4630430cb665914f6d4d94c961f52e0c7d7896ab
Ivan Kyslenko <sg4asite@gmail.com>
2018-01-31 16:56:09 +0200

    Add obj loader

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,56 +1,51 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+import org.omg.CORBA.OBJ_ADAPTER;
 
+import entities.Camera;
+import entities.Entity;
+import entities.Light;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.Renderer;
+import renderEngine.MasterRenderer;
+import renderEngine.OBJLoader;
+import renderEngine.EntityRenderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
-		
+
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-		Renderer renderer  = new Renderer();
-		StaticShader shader = new StaticShader();
+
+
+		RawModel model = OBJLoader.loadObjModel("stall", loader);
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
+
+		ModelTexture texture = staticModel.getTexture();
+		texture.setShineDamper(10);
+		texture.setReflectivity(0.1f);
+		Entity entity = new Entity(staticModel, new Vector3f(0, -5, -25), 0, 0, 0, 1);
+		Light light = new Light(new  Vector3f(0,0,-20), new Vector3f(1,1,1));
 		
-		float[] vertices = {
-				-0.5f, 0.5f, 0f,//v0
-				-0.5f, -0.5f, 0f,//v1
-				0.5f, -0.5f, 0f,//v2
-				0.5f, 0.5f, 0f,//v3
-		};
+		Camera camera = new Camera();
 		
-		int[] indices = {
-				0,1,3,//top left triangle (v0, v1, v3)
-				3,1,2//bottom right triangle (v3, v1, v2)
-		};
-		
-		float[] textureCoords = {
-				0,0, //v0
-				0,1, //v1
-				1,1, //v2
-				1,0  //v3
-		};
-		
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("img1"));
-		TexturedModel texturedModel = new TexturedModel(model, texture);
-		
+		MasterRenderer renderer = new MasterRenderer();
 		while (!Display.isCloseRequested()) {
-			renderer.prepare();
-			shader.start();
-			renderer.render(texturedModel);
-			shader.stop();
+			entity.increaseRotation(0, 0.3f, 0);
+			camera.move();
+			renderer.processEntity(entity);
+			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
 		}
 		
-		shader.cleanUp();
+		renderer.cleanUp();
 		loader.cleanUP();
 		DisplayManager.closeDisplay();
 	}
--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -19,58 +19,62 @@ import org.newdawn.slick.opengl.TextureLoader;
 import models.RawModel;
 
 public class Loader {
-	private List<Integer> vaos = new  ArrayList<Integer>();
-	private List<Integer> vbos = new  ArrayList<Integer>();
-	private List<Integer> textures = new  ArrayList<Integer>();
-	
-	public RawModel loadToVAO(float[] positions, float[] texstureCords, int[] indices) {
+	private List<Integer> vaos = new ArrayList<Integer>();
+	private List<Integer> vbos = new ArrayList<Integer>();
+	private List<Integer> textures = new ArrayList<Integer>();
+
+	public RawModel loadToVAO(float[] positions, float[] texstureCords, float[] normals, int[] indices) {
 		int vaoID = createVAO();
 		bindIndicesBuffer(indices);
 		storeDataInAttributeList(0, 3, positions);
-		storeDataInAttributeList(1, 2,texstureCords);
-		
+		storeDataInAttributeList(1, 2, texstureCords);
+		storeDataInAttributeList(2, 3, normals);
+
 		unbindVAO();
-		
+
 		return new RawModel(vaoID, indices.length);
 	}
-	
-	public int loadTexture(String fileName) { 
+
+	public int loadTexture(String fileName) {
 		Texture texture = null;
 		try {
-			texture = TextureLoader.getTexture("PNG", new FileInputStream("res/"+fileName+".png"));
+			texture = TextureLoader.getTexture("PNG", new FileInputStream("res/" + fileName + ".png"));
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+
 		int textureID = texture.getTextureID();
 		textures.add(textureID);
+//		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+//		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+//		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
+//		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
+		
 		return textureID;
 	}
-	
+
 	public void cleanUP() {
-		for (int vao:vaos) {
+		for (int vao : vaos) {
 			GL30.glDeleteVertexArrays(vao);
 		}
-		for (int vbo:vbos) {
+		for (int vbo : vbos) {
 			GL15.glDeleteBuffers(vbo);
 		}
-		for (int texture:textures) {
+		for (int texture : textures) {
 			GL11.glDeleteTextures(texture);
 		}
 	}
-	
+
 	private int createVAO() {
 		int vaoID = GL30.glGenVertexArrays();
 		vaos.add(vaoID);
 		GL30.glBindVertexArray(vaoID);
-		
+
 		return vaoID;
 	}
-	
+
 	private void storeDataInAttributeList(int attributeNumber, int coordinateSize, float[] data) {
 		int vboID = GL15.glGenBuffers();
 		vbos.add(vboID);
@@ -80,11 +84,11 @@ public class Loader {
 		GL20.glVertexAttribPointer(attributeNumber, coordinateSize, GL11.GL_FLOAT, false, 0, 0);
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
 	}
-	
+
 	private void unbindVAO() {
 		GL30.glBindVertexArray(0);
 	}
-	
+
 	private void bindIndicesBuffer(int[] indices) {
 		int vboID = GL15.glGenBuffers();
 		vbos.add(vboID);
@@ -92,20 +96,20 @@ public class Loader {
 		IntBuffer buffer = storeDatatInIntBuffer(indices);
 		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
 	}
-	
+
 	private IntBuffer storeDatatInIntBuffer(int[] data) {
 		IntBuffer buffer = BufferUtils.createIntBuffer(data.length);
 		buffer.put(data);
 		buffer.flip();
-		
+
 		return buffer;
 	}
-	
+
 	private FloatBuffer storeDataInFloatBuffer(float[] data) {
 		FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
 		buffer.put(data);
 		buffer.flip();
-		
+
 		return buffer;
 	}
 }
